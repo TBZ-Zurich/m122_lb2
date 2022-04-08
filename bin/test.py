0a9a38668a51161996a7a303ee2e65ab6f7a1deb
Loris Polenz <contact@lorispolenz.com>
2022-04-08 07:53:22 +0200

    feat(git): enhanced arg handling
    arg handling was enhance  with default values that get used in the script

--- bin/test.py
+++ bin/test.py
@@ -1,19 +1,41 @@
+#! /bin/python3
 from git import Repo
 import csv
-from datetime import date
 import os
+import argparse
 
-fields = ["Zielverzeichnis", "Datum", "Commit-Hash", "Author"]
 
-filename = "university_records.csv"
-baseDir = '/Users/lorispolenz/Documents/GitHub/tbz/m122/'
+FIELDS = ["Zielverzeichnis", "Datum", "Commit-Hash", "Author"]
+
+FILENAME = "gitlogs.csv"
+TARGET_DIRECTORY = ''
+CWD = os.getcwd()
 rows = []
 
+# Handle arguments
+parser = argparse.ArgumentParser()
 
-my_list = os.listdir(baseDir)
+parser.add_argument(
+    "-o", "--Output", help="Ouput location and filename", default="gitlogs.csv")
+parser.add_argument("-d", "--Directory",
+                    help="Target directory with the git repos", required=True)
+
+args = parser.parse_args()
+
+OUTPUT_LOCATION = args.Output
+TARGET_DIRECTORY = args.Directory
+
+
+def dtToDateString(datetime):
+    dateStr = ["%s" % datetime.year,
+               ("0%s" % datetime.month)[-2:], ("0%s" % datetime.day)[-2:]]
+    return "".join(dateStr)
+
+
+my_list = os.listdir(TARGET_DIRECTORY)
 
 for dire in my_list:
-    directory = baseDir + dire
+    directory = TARGET_DIRECTORY + dire
     if os.path.isdir(directory):
 
         try:
@@ -25,26 +47,20 @@ for dire in my_list:
         repo = Repo(directory)
         commits = list(repo.iter_commits())
 
-        print(commits)
-
         for commit in commits:
             row = []
 
             row.append(dire)
-            row.append(str(date.today()).replace("-", ""))
+            row.append(dtToDateString(commit.authored_datetime))
             row.append(commit.hexsha)
             row.append(commit.author)
 
             rows.append(row)
 
 
-# writing to csv file
-with open(filename, 'w') as csvfile:
-    # creating a csv writer object
+with open(str(OUTPUT_LOCATION), 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
 
-    # writing the fields
-    csvwriter.writerow(fields)
+    csvwriter.writerow(FIELDS)
 
-    # writing the data rows
     csvwriter.writerows(rows)
