a015a9b48bcfc80da81c3f2e290ee3116666a310
Loris Polenz <contact@lorispolenz.com>
2022-05-09 18:16:31 +0200

    feat(script2): add logging.py

---
+++ bin/logger.py
@@ -0,0 +1,33 @@
+import logging
+
+# Create and configure logger
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.INFO)
+
+# Test messages
+# logger.debug("Harmless debug Message")
+# logger.info("Just an information")
+# logger.warning("Its a Warning")
+# logger.error("Did you try to divide by zero")
+# logger.critical("Internet is down")
+
+
+def logInformation(information):
+    logString = "INFO: " + information
+
+    print(logString)
+    logger.info(logString)
+
+
+def logWarning(warning):
+    logString = "WARN   : " + warning
+
+    print(logString)
+    logger.info(logString)
