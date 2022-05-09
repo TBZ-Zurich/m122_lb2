a3e4db6e56fcd5060abeb3aa4f2de34adf0a3cd1
Lenny Lam <git@lennylam.dev>
2022-05-09 22:26:14 +0200

    Update git_clone_update_repos.py

--- bin/git_clone_update_repos.py
+++ bin/git_clone_update_repos.py
@@ -1,4 +1,4 @@
-import argparse, os, git
+import argparse, os, git, logger, shutil, glob
 from git import Repo
 
 
@@ -7,7 +7,7 @@ from git import Repo
 parser = argparse.ArgumentParser(description="This clones all your git repos to your doirectory",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d", "--dir",
-                    help="You can enter your base directory here", required=FALSE)
+                    help="You can enter your base directory here", required=True)
 args = parser.parse_args()
 BASE_DIR = args.dir
 
@@ -31,3 +31,12 @@ with open("gitclone.config") as config_file:
             g.pull()
         elif not os.path.isdir(BASE_DIR + '/' + config_line[1]):
             Repo.clone_from(config_line[0], BASE_DIR + '/' +config_line[1])
+
+    dir_list = os.listdir(BASE_DIR + '/') 
+    print(dir_list)
+    for count in dir_list:
+        print(count)
+        if not(count in config_file.read()):
+            print("not avaiilble")
+        else:
+            print("not avaiulble") 
