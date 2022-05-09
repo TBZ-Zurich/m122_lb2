11a688161841e2763cd59571db1beb0de3e4e3f8
Lenny Lam <git@lennylam.dev>
2022-05-09 23:01:43 +0200

    Updated script
    -Added logger
    -Added delete/rm function

--- bin/git_clone_update_repos.py
+++ bin/git_clone_update_repos.py
@@ -14,9 +14,9 @@ BASE_DIR = args.dir
 # Checks if the config file exists
 
 if os.path.exists("gitclone.config"):
-    print("The configfile does exists")
+    logger.logInformation("The configfile does exists")
 else:
-    print("the config file does not exists, please read the readme.md file")
+    logger.logError("The config file does not exists, please read the readme.md file")
     quit()
 
 # Reads the config file
@@ -24,19 +24,20 @@ else:
 with open("gitclone.config") as config_file:
     while (line := config_file.readline().rstrip()):
         config_line = line.split()
-        print(config_line[0] + ' ' + config_line[1])
-        if os.path.isdir(BASE_DIR + '/' + config_line[1]):
+        logger.logInformation(config_line[0] + ' ' + config_line[1])
+        if os.path.isdir(BASE_DIR + '/' + config_line[1]): # Checks if repo is already cloned
             g = git.cmd.Git(BASE_DIR + '/' + config_line[1])
             g.fetch()
             g.pull()
-        elif not os.path.isdir(BASE_DIR + '/' + config_line[1]):
-            Repo.clone_from(config_line[0], BASE_DIR + '/' +config_line[1])
+        elif not os.path.isdir(BASE_DIR + '/' + config_line[1]): 
+            Repo.clone_from(config_line[0], BASE_DIR + '/' + config_line[1])
+
+    # If the folder is not in config file, it deletes the folder ion the BASE_DIR
 
     dir_list = os.listdir(BASE_DIR + '/') 
-    print(dir_list)
     for count in dir_list:
-        print(count)
-        if not(count in config_file.read()):
-            print("not avaiilble")
-        else:
-            print("not avaiulble") 
+        logger.logInformation('Checking if dir ' + count + ' exists')
+        with open('gitclone.config') as config_file:
+            if not(count in config_file.read()):
+                shutil.rmtree(BASE_DIR + '/' + count)
+                logger.logInformation("Removed " + count)
